e458db1ae82c2e1adb3f0ccec4b98d195591f954
Marie Katrine Ekeberg <mke@themkat.net>
2023-03-04 21:55:17 +0100

    WIP. Initial chatgpt requests

--- story_generator/src/openai/mod.rs
+++ story_generator/src/openai/mod.rs
@@ -106,3 +106,72 @@ pub async fn do_image_generation_request(
         .url
         .clone())
 }
+
+// ChatGPT API requests
+#[derive(Debug, Serialize, Deserialize)]
+struct OpenAiChatRequest {
+    model: String,
+    messages: Vec<OpenAiChatMessage>,
+    temperature: f32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct OpenAiChatMessage {
+    role: String,
+    content: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct OpenAiChatResponse {
+    choices: Vec<OpenAiChatChoice>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct OpenAiChatChoice {
+    message: OpenAiChatMessage,
+}
+
+async fn do_chat_request(
+    api_token: &String,
+    earlier_messages: &Vec<String>,
+) -> Result<String, reqwest::Error> {
+    let client = reqwest::Client::builder()
+        .timeout(Duration::from_secs(720))
+        .build()
+        .expect("Could not create http client");
+
+    let request = OpenAiChatRequest {
+        model: "gpt-3.5-turbo".to_string(),
+        messages: earlier_messages
+            .iter()
+            .enumerate()
+            .map(|(ind, msg)| OpenAiChatMessage {
+                // assume that even indices 0,2,4 etc. is the user. The others are the replies/assistant
+                role: if ind % 2 == 0 {
+                    "user".to_string()
+                } else {
+                    "assistant".to_string()
+                },
+                content: msg.to_string(),
+            })
+            .collect(),
+        temperature: (rand::thread_rng().gen_range(0..9) as f32) / 10.0,
+    };
+
+    let result = client
+        .post("https://api.openai.com/v1/chat/completions")
+        .header("Authorization", format!("Bearer {api_token}"))
+        .header("Content-Type", "application/json")
+        .body(serde_json::to_string(&request).expect("Could not write json to string"))
+        .send()
+        .await?
+        .json::<OpenAiChatResponse>()
+        .await?;
+
+    Ok(result
+        .choices
+        .first()
+        .expect("No chat message in result")
+        .message
+        .content)
+}
